14b1263004f8c27175d57268f8247db2597dec7e
nictru <nictru32@gmail.com>
2025-06-20 18:27:03 +0200

    Add translated section labels

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,36 +20,159 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Prospectives',
+					translations: {
+						de: 'Studieninteressierte',
+					},
 					items: [
-						{ label: 'Was ist Bioinformatik?', link: 'prospectives/what-is-bioinformatics' },
-						{ label: 'Studieninhalte', link: 'prospectives/program-contents' },
-						{ label: 'Schnuppermaterialien', link: 'prospectives/study-materials' },
-						{ label: 'Aufnahmeverfahren', link: 'prospectives/application-process' },
-						{ label: 'Berufsaussichten', link: 'prospectives/career-perspectives' },
-						{ label: 'Für Master-Interessierte', link: 'prospectives/master-students' },
-						{ label: 'München und andere Standorte', link: 'prospectives/munich_vs_other_locations' },
+						{ 
+							label: 'What is Bioinformatics?',
+							translations: {
+								de: 'Was ist Bioinformatik?',
+							},
+							link: 'prospectives/what-is-bioinformatics' 
+						},
+						{ 
+							label: 'Program Contents',
+							translations: {
+								de: 'Studieninhalte',
+							},
+							link: 'prospectives/program-contents' 
+						},
+						{ 
+							label: 'Study Materials',
+							translations: {
+								de: 'Schnuppermaterialien',
+							},
+							link: 'prospectives/study-materials' 
+						},
+						{ 
+							label: 'Application Process',
+							translations: {
+								de: 'Aufnahmeverfahren',
+							},
+							link: 'prospectives/application-process' 
+						},
+						{ 
+							label: 'Career Perspectives',
+							translations: {
+								de: 'Berufsaussichten',
+							},
+							link: 'prospectives/career-perspectives' 
+						},
+						{ 
+							label: 'For Master Students',
+							translations: {
+								de: 'Für Master-Interessierte',
+							},
+							link: 'prospectives/master-students' 
+						},
+						{ 
+							label: 'Munich vs Other Locations',
+							translations: {
+								de: 'München und andere Standorte',
+							},
+							link: 'prospectives/munich_vs_other_locations' 
+						},
 					],
 				},
 				{
 					label: 'Student Council',
+					translations: {
+						de: 'Fachschaft',
+					},
 					items: [
-						{ label: 'Über uns', link: 'student-council/about-us' },
-						{ label: 'Gremien', link: 'student-council/committees' },
-						{ label: 'Veranstaltungen', link: 'student-council/events' },
-						{ label: 'Mitmachen', link: 'student-council/join-us' },
-						{ label: 'Organisation', link: 'student-council/organization' },
+						{ 
+							label: 'About Us',
+							translations: {
+								de: 'Über uns',
+							},
+							link: 'student-council/about-us' 
+						},
+						{ 
+							label: 'Committees',
+							translations: {
+								de: 'Gremien',
+							},
+							link: 'student-council/committees' 
+						},
+						{ 
+							label: 'Events',
+							translations: {
+								de: 'Veranstaltungen',
+							},
+							link: 'student-council/events' 
+						},
+						{ 
+							label: 'Join Us',
+							translations: {
+								de: 'Mitmachen',
+							},
+							link: 'student-council/join-us' 
+						},
+						{ 
+							label: 'Organization',
+							translations: {
+								de: 'Organisation',
+							},
+							link: 'student-council/organization' 
+						},
 					],
 				},
 				{
 					label: 'Students',
+					translations: {
+						de: 'Studierende',
+					},
 					items: [
-						{ label: 'Für Studierende', link: 'students/index' },
-						{ label: 'Übergang Bachelor-Master', link: 'students/bachelor-master-transition' },
-						{ label: 'Prüfungsordnung', link: 'students/examination-regulations' },
-						{ label: 'Linksammlung', link: 'students/link-collection' },
-						{ label: 'Ersti-Onboarding', link: 'students/onboarding' },
-						{ label: 'Auslandsaufenthalt', link: 'students/study-abroad' },
-						{ label: 'Abschlussarbeit', link: 'students/thesis' },
+						{ 
+							label: 'For Students',
+							translations: {
+								de: 'Für Studierende',
+							},
+							link: 'students/index' 
+						},
+						{ 
+							label: 'Bachelor-Master Transition',
+							translations: {
+								de: 'Übergang Bachelor-Master',
+							},
+							link: 'students/bachelor-master-transition' 
+						},
+						{ 
+							label: 'Examination Regulations',
+							translations: {
+								de: 'Prüfungsordnung',
+							},
+							link: 'students/examination-regulations' 
+						},
+						{ 
+							label: 'Link Collection',
+							translations: {
+								de: 'Linksammlung',
+							},
+							link: 'students/link-collection' 
+						},
+						{ 
+							label: 'First-Year Onboarding',
+							translations: {
+								de: 'Ersti-Onboarding',
+							},
+							link: 'students/onboarding' 
+						},
+						{ 
+							label: 'Study Abroad',
+							translations: {
+								de: 'Auslandsaufenthalt',
+							},
+							link: 'students/study-abroad' 
+						},
+						{ 
+							label: 'Thesis',
+							translations: {
+								de: 'Abschlussarbeit',
+							},
+							link: 'students/thesis' 
+						},
 					],
 				},
 			],
